2b595d599a357ccfccb2f411002695be1393bc11
JAUROYON maxime <maxime.jauroyon@gmail.com>
2022-04-29 16:47:38 +0200

    one more

--- bd6.sql
+++ bd6.sql
@@ -287,4 +287,33 @@ $function$;
 
 ALTER TABLE trading ADD CONSTRAINT check_category CHECK (category_mismatches_trading() = 0);
 
+
+CREATE OR REPLACE FUNCTION type_mismatches()
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    RETURN (
+        SELECT COUNT(*)
+        FROM
+            (SELECT *
+            FROM shipments
+            WHERE shipment_type = 'court'
+            AND distance >= 1000
+            UNION
+            SELECT *
+            FROM shipments
+            WHERE shipment_type = 'moyen'
+            AND (distance < 1000 OR distance >= 2000)
+            UNION
+            SELECT *
+            FROM shipments
+            WHERE shipment_type = 'long'
+            AND distance < 2000) as D);
+END;
+$function$;
+
+ALTER TABLE shipments ADD CONSTRAINT check_type CHECK (type_mismatches() = 0);
+
+
 \i 'load.sql';
