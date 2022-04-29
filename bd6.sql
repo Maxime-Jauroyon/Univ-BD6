6615a95c6b56f5a93cf8adbe1884be3c7cc9a950
JAUROYON maxime <maxime.jauroyon@gmail.com>
2022-04-29 16:57:55 +0200

    and another one

--- bd6.sql
+++ bd6.sql
@@ -315,5 +315,21 @@ $function$;
 
 ALTER TABLE shipments ADD CONSTRAINT check_type CHECK (type_mismatches() = 0);
 
+CREATE OR REPLACE FUNCTION category_mismatches_Intercontinental()
+ RETURNS integer
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    RETURN (
+        SELECT
+             COUNT(*)
+        FROM shipments
+        NATURAL JOIN ships
+        WHERE class = 'Intercontinental'
+        AND ship_category <> 5);
+END;
+$function$;
+
+ALTER TABLE shipments ADD CONSTRAINT check_category_Intercontinental CHECK (category_mismatches_Intercontinental() = 0);
 
 \i 'load.sql';
